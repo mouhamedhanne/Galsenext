3580ccf5b6c7555ef18168526940bf22f9a5e57f
mouhamedhanne <mouhamedhanne72@gmail.com>
2024-08-24 22:14:57 +0000

    Setup project

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,74 @@
+import { env } from "@/src/lib/env";
+import { prisma } from "@/src/lib/prisma";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import NextAuth, { AuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+const FixedPrismaAdapter = PrismaAdapter(prisma) as any;
+
+export const authOptions: AuthOptions = {
+  session: {
+    strategy: "jwt",
+  },
+  adapter: FixedPrismaAdapter,
+  theme: {
+    logo: "/images/onlineclass.png",
+  },
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      profile(profile) {
+        return {
+          id: profile.sub,
+          name: `${profile.given_name} ${profile.family_name}`,
+          email: profile.email,
+          image: profile.picture,
+          role: profile.role ? profile.role : "user",
+          emailVerified: null,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          level: profile.level ? profile.level : "",
+          specialty: profile.specialty ? profile.specialty : "",
+          xp: profile.xp ? profile.xp : 0,
+          isOnboarded: false,
+        };
+      },
+    }),
+  ],
+
+  callbacks: {
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.id as string;
+        session.user.role = token.role as string;
+        session.user.isOnboarded = token.isOnboarded as boolean;
+      }
+      return session;
+    },
+    async jwt({ token, user, account }) {
+      if (user) {
+        token.id = user.id;
+        token.role = user.role;
+        token.level = user.level;
+        token.specialty = user.specialty;
+        token.xp = user.xp;
+        token.isOnboarded = user.isOnboarded;
+      }
+
+      if (!account) {
+        const dbUser = await prisma.user.findUnique({
+          where: { id: token.id as string },
+          select: { isOnboarded: true },
+        });
+        if (dbUser) {
+          token.isOnboarded = dbUser.isOnboarded;
+        }
+      }
+
+      return token;
+    },
+  },
+};
+
+export default NextAuth(authOptions);
