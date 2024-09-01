8be4e3981b751ee452f77f3400c1a4f13f8dc46d
mouhamedhanne <mouhamedhanne72@gmail.com>
2024-09-01 00:19:01 +0000

    Fixed bug prod

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -14,7 +14,7 @@ export function ThemeToggle() {
       onClick={() => setTheme(theme === "light" ? "dark" : "light")}
     >
       <Sun className="h-[1.5rem] w-[1.3rem] dark:hidden" />
-      <Moon className="hidden h-5 w-5 dark:block" />
+      <Moon className="hidden h-8 w-8 dark:block" />
       <span className="sr-only">Toggle theme</span>
     </Button>
   );
