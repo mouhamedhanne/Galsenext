2d834293f63a5d0c8b5cd0642a4fddd140a2aaa2
mouhamedhanne <mouhamedhanne72@gmail.com>
2024-09-01 12:23:38 +0000

    Update landing page

--- app/[locale]/Providers.tsx
+++ app/[locale]/Providers.tsx
@@ -1,20 +1,40 @@
 "use client";
 
-import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, createContext, useContext } from "react";
+import { SessionProvider } from "next-auth/react";
 import { Toaster } from "react-hot-toast";
 import { AppProgressBar as ProgressBar } from "next-nprogress-bar";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { I18nProviderClient } from "@/locales/client";
-import { usePathname } from "next/navigation";
 
 const queryClient = new QueryClient();
 
-export const Providers = (props: PropsWithChildren<{ locale: string }>) => {
-  const pathname = usePathname();
-  const isLandingPage = pathname === "/";
+const ThemeExclusionContext = createContext(false);
 
+export const useThemeExclusion = () => useContext(ThemeExclusionContext);
+
+export const ExcludeFromTheme = ({ children }: PropsWithChildren) => (
+  <ThemeExclusionContext.Provider value={true}>
+    {children}
+  </ThemeExclusionContext.Provider>
+);
+
+const CustomThemeProvider = ({ children }: PropsWithChildren) => {
+  const isExcluded = useThemeExclusion();
+
+  if (isExcluded) {
+    return <>{children}</>;
+  }
+
+  return (
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      {children}
+    </ThemeProvider>
+  );
+};
+
+export const Providers = (props: PropsWithChildren<{ locale: string }>) => {
   return (
     <>
       <SessionProvider>
@@ -27,17 +47,7 @@ export const Providers = (props: PropsWithChildren<{ locale: string }>) => {
         />
         <QueryClientProvider client={queryClient}>
           <I18nProviderClient locale={props.locale}>
-            {isLandingPage ? (
-              props.children
-            ) : (
-              <ThemeProvider
-                attribute="class"
-                defaultTheme="system"
-                enableSystem
-              >
-                {props.children}
-              </ThemeProvider>
-            )}
+            <CustomThemeProvider>{props.children}</CustomThemeProvider>
           </I18nProviderClient>
         </QueryClientProvider>
       </SessionProvider>
